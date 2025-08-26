56f104c08ad8fb00297be86720037c4a7ee98a6e
Me <ericrvaa@gmail.com>
2025-08-26 04:15:37 -0400

    add test

---
+++ src/shaders/roadLatticeShader.turn.test.js
@@ -0,0 +1,78 @@
+import { computeTurn } from './roadLatticeShader.js';
+
+const GRID_SIZE = 10;
+
+function makeCar(dir, direction, road, lane, pos) {
+  return { dir, direction, road, lane, pos };
+}
+
+function makeIntersection(x, y) {
+  return { x, y };
+}
+
+describe('computeTurn', () => {
+  test('left turn from horizontal', () => {
+    const car = makeCar('h', 1, 3, 1, 0.3);
+    const inter = makeIntersection(5, 4);
+    const result = computeTurn(car, inter, 'left', GRID_SIZE);
+    expect(result).toEqual({
+      dir: 'v',
+      direction: -1,
+      road: 4, // intersection.y
+      lane: 1,
+      pos: 4 / GRID_SIZE,
+    });
+  });
+
+  test('right turn from horizontal', () => {
+    const car = makeCar('h', -1, 3, 0, 0.3);
+    const inter = makeIntersection(5, 4);
+    const result = computeTurn(car, inter, 'right', GRID_SIZE);
+    expect(result).toEqual({
+      dir: 'v',
+      direction: -1,
+      road: 4, // intersection.y
+      lane: 1,
+      pos: 4 / GRID_SIZE,
+    });
+  });
+
+  test('left turn from vertical', () => {
+    const car = makeCar('v', 1, 2, 0, 0.2);
+    const inter = makeIntersection(6, 7);
+    const result = computeTurn(car, inter, 'left', GRID_SIZE);
+    expect(result).toEqual({
+      dir: 'h',
+      direction: -1,
+      road: 6, // intersection.x
+      lane: 0,
+      pos: 6 / GRID_SIZE,
+    });
+  });
+
+  test('right turn from vertical', () => {
+    const car = makeCar('v', -1, 2, 1, 0.2);
+    const inter = makeIntersection(6, 7);
+    const result = computeTurn(car, inter, 'right', GRID_SIZE);
+    expect(result).toEqual({
+      dir: 'h',
+      direction: -1,
+      road: 6, // intersection.x
+      lane: 0,
+      pos: 6 / GRID_SIZE,
+    });
+  });
+
+  test('straight', () => {
+    const car = makeCar('h', 1, 3, 1, 0.3);
+    const inter = makeIntersection(5, 4);
+    const result = computeTurn(car, inter, 'straight', GRID_SIZE);
+    expect(result).toEqual({
+      dir: 'h',
+      direction: 1,
+      road: 3,
+      lane: 1,
+      pos: 0.3,
+    });
+  });
+});
